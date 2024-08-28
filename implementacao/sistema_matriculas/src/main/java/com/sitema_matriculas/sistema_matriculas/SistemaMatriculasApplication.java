e6bbb37c00a0d0abd7f807bbf84084212b06da07
Rubens <rubensmarcelo31@gmail.com>
2024-08-27 21:06:58 -0300

    init: spring project

---
+++ implementacao/sistema_matriculas/src/main/java/com/sitema_matriculas/sistema_matriculas/SistemaMatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.sitema_matriculas.sistema_matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMatriculasApplication.class, args);
+	}
+
+}
---
+++ implementacao/sistema_matriculas/src/test/java/com/sitema_matriculas/sistema_matriculas/SistemaMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitema_matriculas.sistema_matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
